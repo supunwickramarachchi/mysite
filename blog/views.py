a7a87edcfbd19da010b5561b7bbb89db5d956670
supunwickramarachchi <supun9718wic@gmail.com>
2025-08-07 13:51:19 +0530

    Added full text search for the blog with trigram similarity

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 from django.views.generic import ListView
 
@@ -88,10 +88,12 @@ def post_comment(request, post_id):
 def post_search(request):
     form = SearchForm()
     query = None 
-    results = []
+    results = [] 
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+            # search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            # search_query = SearchQuery(query)
+            results = Post.published.annotate(similarity=TrigramSimilarity('title', query) + TrigramSimilarity('body', query),).filter(similarity__gte=0.1).order_by('-similarity')
     return render(request, 'blog/post/search.xhtml', {'form': form, 'query': query, 'results': results})
